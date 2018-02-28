65637e57c986956c2e5a0b203fa0ffe09a2fb1ed
Tobias Dühr <nerd@tobiasduehr.com>
2018-02-28 22:18:59 +0100

    ask user to install

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 yarn production
-rm -rf /tmp/minisweeper-deploy
-mkdir /tmp/minisweeper-deploy
-cp -r ./web/* /tmp/minisweeper-deploy
 git checkout gh-pages
-cp -r /tmp/minisweeper-deploy/* ./
+mv ./web/* ./
+rm -rf ./web
 git status
--- src/js/app.js
+++ src/js/app.js
@@ -9,10 +9,19 @@ render(
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('sw.js').then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  }
+}
+
+window.addEventListener('beforeinstallprompt', function(e) {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+    
+    window.deferredPrompt = e;
+    
+    return false;
+});
